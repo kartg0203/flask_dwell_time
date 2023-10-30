9051ef599f154e55a97e2cef3611690155cb100a
kartg0203 <kartg0203@gmail.com>
2023-10-30 18:39:25 +0800

    測試: db改用pool

--- app/db.py
+++ app/db.py
@@ -1,15 +1,25 @@
 import psycopg2
+from psycopg2 import pool
 import config
 
 def connect_test():
     """ Connect to the PostgreSQL database server """
     conn = None
     try:
-        # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(database=config.DB_DATABASE, user=config.DB_USERNAME, password=config.DB_PASSWORD, host=config.DB_HOST, port=config.DB_PORT, options=f"-c search_path={config.DB_SCHEMA}")
-        with conn.cursor() as cur:
+        # conn = psycopg2.connect(database=config.DB_DATABASE, user=config.DB_USERNAME, password=config.DB_PASSWORD, host=config.DB_HOST, port=config.DB_PORT, options=f"-c search_path={config.DB_SCHEMA}")
+        db_config = {
+            'database': config.DB_DATABASE,
+            'user': config.DB_USERNAME,
+            'password': config.DB_PASSWORD,
+            'host': config.DB_HOST,
+            'port': config.DB_PORT,
+            'options': f"-c search_path={config.DB_SCHEMA}"
+        }
+        connection_pool = pool.SimpleConnectionPool(1, 10, **db_config)
+        conn = connection_pool.getconn()
 
+        with conn.cursor() as cur:
     	# execute a statement
             print('PostgreSQL database version:')
             cur.execute('SELECT * FROM eo_sales_location')
@@ -21,8 +31,8 @@ def connect_test():
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
-        if conn is not None:
-            conn.close()
+        if conn:
+            conn.closeall()
             print('Database connection closed.')
 
 if __name__ == '__main__':
